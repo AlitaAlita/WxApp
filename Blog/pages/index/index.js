343b1f1415f94af12de1f8458a8a75d656450bed
RenJian <23418702@qq.com>
2020-03-06 18:03:12 +0800

    博客列表首页展示优化（解决关联对象冲突）

--- Blog/pages/index/index.js
+++ Blog/pages/index/index.js
@@ -6,7 +6,7 @@ Page({
   data: {
    articles:[],
    isLoadingMore:false,
-   currentPage:1,
+   currentPage:0,
    info:''
   },
   //事件处理函数
@@ -18,16 +18,22 @@ Page({
   loadArticles: function (){
     var that = this
     wx.request({
-      url: "http://localhost:8080/api/blogs",
+      url: 'http://localhost:8080/api/blogs?page='+that.data.currentPage,
       success: (res) => {
         if (res.data.msg === 'success') {
+          if (res.data.result.content.length==0){
+            that.setData({
+              isLoadingMore:false,
+              info:"无更多博客"
+            });
+          }
           console.log(res.data.result.content)
           that.setData({
-            articles:res.data.result.content
-          })
+            articles: that.data.articles.concat(res.data.result.content)
+          });
         } else {
           that.setData({
-            info: '加载文章列表失败，请重试'
+            info: '加载博客列表失败，请重试'
           })
         }
         wx.hideLoading()
@@ -37,8 +43,19 @@ Page({
   },
   onLoad: function () {
     wx.showLoading({
-      title: '文章加载中...'
+      title: '博客加载中...'
     })
+    this.loadArticles()
+  },
+  onReachBottom: function () {
+    this.data.currentPage++
+    if (this.data.isLoadingMore && this.data.currentPage > 20) {
+      // 最多只能加载20页
+      this.data.isLoadingMore = false
+      this.data.info = '无更多博客'
+      return
+    }
+    this.data.isLoadingMore = true
     this.loadArticles()
   }
 })
